7d47f39c6074b609e0285b510004ac3fbefc67a6
386158 <386158@IZMINTMRKBTH03>
2024-03-04 17:40:34 +0300

    Class 14 / 3:32

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("This area can not be empty.");
+            RuleFor(x => x.Image).NotEmpty().WithMessage("This area can not be empty.");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("At least 50 characters needed.");
+            RuleFor(x => x.Description).MinimumLength(1500).WithMessage("Can not be more than 1500 characters.");
+        }
+    }
+}
