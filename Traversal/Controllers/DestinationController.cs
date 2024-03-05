fc6aa4c57b9cf5cecc8cfde031a9cc264fdbc13d
386158 <386158@IZMINTMRKBTH03>
2024-03-05 17:24:38 +0300

    20/08:27

---
+++ Traversal/Controllers/DestinationController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EFDestinationDAL());
+        public IActionResult Index()
+        {
+            var destinations = destinationManager.GetAll();
+            return View(destinations);
+        }
+    }
+}
